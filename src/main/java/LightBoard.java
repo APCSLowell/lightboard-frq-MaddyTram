3ab016779e92f2dc94668add4d3fcf312eb6a57b
MaddyTram <116767768+MaddyTram@users.noreply.github.com>
2025-04-29 12:09:50 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -11,7 +11,16 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     /* to be implemented in part (a) */
-
+    lights = new boolean[numRows][numCols];
+    for(int r = 0; r < numRows; r++){
+      for(int c = 0; c < numCols; c++) {
+        if(Math.random() < 0.4) {
+          light[r][c] = true;
+        } else {
+          light[r][c] = false;
+        }
+      }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -21,8 +30,19 @@ public class LightBoard
   public boolean evaluateLight(int row, int col)
   {
     /* to be implemented in part (b) */
-   
- 
+    int on = 0;
+    for(int r = 0; r < lights.length; r++){
+      if(lights[r][col]) {
+        num++;
+      }
+    }
+    if(lights[row][col] && on % 2 == 0) {
+      return false;
+    }
+    if(!lights[row][col] && on % 3 == 0) {
+      return true;
+    }
+    returnh light[row][col];
   }
   public boolean[][] getLights()
   {
